70f2b14e8a5b941ac37c69365a6f2ef7a798356d
FourAndHalf <jinsoneb@gmail.com>
2025-04-12 11:00:34 +0530

    A running code of hello world in rust

--- rust/helloworld/src/main.rs
+++ rust/helloworld/src/main.rs
@@ -1,7 +1,10 @@
 use std::env;
 use std::collections::HashMap;
 use once_cell::sync::Lazy;
-use rand:: rng;
+use rand:: {
+    rng,
+    prelude::IndexedRandom
+};
 
 // Hashmap of the values of Hello, World!
 static GREETING: Lazy<HashMap<&str, &str>> = Lazy::new(|| {
@@ -13,30 +16,34 @@ static GREETING: Lazy<HashMap<&str, &str>> = Lazy::new(|| {
         ("he", "שלום, עולם!"),
         ("fr", "Bonjour, le monde!"),
     ])
-})
-
-fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-
-    greet(args.unwrap());
-}
+});
 
 /*  If no language is given as argument,
     a random language is taken from key list
     and shown the greetings for that language
 */
-fn greet(mut lang: String) -> none {
-    let mut code = &lang;
-    
-    if lang.is_empty() {
-        code = get_random_key();
-    }
 
-    println!("{}", GREETING.get(code))
-}
-
-fn get_random_key() -> String {
+fn get_random_key() -> &'static str {
     let keys: Vec<_> = GREETING.keys().copied().collect();
     let mut rng = rng();
-    keys.choose(&mut rng).copied();
+
+    keys.choose(&mut rng).copied().unwrap_or("en")
+}
+
+fn greet(lang: Option<String>) {
+    let code = match lang {
+        Some(l) if !l.is_empty() => l,
+        _ => get_random_key().to_string(),
+    };
+
+    match GREETING.get(code.as_str()) {
+        Some(message) => println!("{}", message),
+        None => println!("Unknown language code."),
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let lang = args.get(0).cloned();
+    greet(lang);
 }
